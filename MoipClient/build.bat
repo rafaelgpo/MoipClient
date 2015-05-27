ec156abacc4a73afc63e041135ffad675a816787
rafaelgpo <rafaelgpo@gmail.com>
2015-05-27 11:50:15 -0300

    Nuget files

---
+++ MoipClient/build.bat
@@ -0,0 +1,17 @@
+﻿@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild MoipClient.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+REM Package
+mkdir Build
+cmd /c %nuget% pack "MoipClient\Moip.csproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
