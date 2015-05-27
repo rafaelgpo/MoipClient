2da29fbcb481c31fc734f025250c29eb474dbd90
rafaelgpo <rafaelgpo@gmail.com>
2015-05-27 16:54:07 -0300

    Atualiza versão

--- MoipClient/build.bat
+++ MoipClient/build.bat
@@ -14,10 +14,10 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild MoipClient\MoipClient.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild MoipClient\MoipClient.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
-mkdir Build\lib\net35
+mkdir Build\lib\net40
 
 %nuget% pack "MoipClient\MoipClient.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
